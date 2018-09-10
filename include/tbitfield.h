3c3c1529e69bbad48ced130b8acd8778722ec2f7
letnonesurvive <hippiman1221@gmail.com>
2018-09-10 23:55:09 +0300

    Некоторые методы были заполнены, тесты проведены

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -30,10 +30,10 @@ public:
   ~TBitField();                      //                                    (#С)
 
   // доступ к битам
+  int  GetBit(const int n) const; // получить значение бита               (#Л1)
   int GetLength(void) const;      // получить длину (к-во битов)           (#О)
   void SetBit(const int n);       // установить бит                       (#О4)
   void ClrBit(const int n);       // очистить бит                         (#П2)
-  int  GetBit(const int n) const; // получить значение бита               (#Л1)
 
   // битовые операции
   int operator==(const TBitField &bf) const; // сравнение                 (#О5)
--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,10 +9,28 @@
 
 TBitField::TBitField(int len)
 {
+	if (len > 0)
+	{
+		BitLen = len;
+		MemLen = len / (sizeof(TELEM) * 8);
+		pMem = new TELEM[MemLen];
+		for (int i = 0; i <= MemLen; i++)
+		{
+			pMem[i] = 0;
+		}
+	}
+	if (len < 0)
+	{
+		throw "negative len";
+	}
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+	pMem = new TELEM[bf.BitLen];
+	for (int i = 0; i < bf.BitLen; i++)
+		pMem[i] = bf.pMem[i];
+	BitLen = bf.BitLen;
 }
 
 TBitField::~TBitField()
@@ -21,66 +39,87 @@ TBitField::~TBitField()
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+	return MemLen;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+		TELEM mask = 1;
+		int position = n % (sizeof(TELEM) * 8);//на какой позиции находится бит в ячейке массива
+		mask = mask << position;
+		if(GetBit(n)==1)
+			return ~mask;
+		if (GetBit(n) == 0)
+			return mask;
 }
 
-// доступ к битам битового поля
-
+//// доступ к битам битового поля
+//
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+  return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+	int num = n / BitLen;//в какой ячейке массива содержится бит
+	int position = n % (sizeof(TELEM) * 8);//на какой позиции находится бит в ячейке массива
+	pMem[num] = pMem[num] | GetMemMask(pMem[num]);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+	int num = n / BitLen;
+	int position = n % (sizeof(TELEM) * 8);
+	pMem[num] = pMem[num] & ~(GetMemMask(pMem[num]));
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+	int a = 1;
+	int num = n / BitLen;//в какой ячейке массива содержится бит
+	int position = n % (sizeof(TELEM) * 8);//на какой позиции находится бит в ячейке массива
+	a = a << position;
+	if (pMem[num] & a == a)
+		return 1;
+	if (pMem[num] & a == 0)
+		return 0;
 }
-
-// битовые операции
-
-TBitField& TBitField::operator=(const TBitField &bf) // присваивание
-{
-}
-
-int TBitField::operator==(const TBitField &bf) const // сравнение
-{
-  return 0;
-}
-
-int TBitField::operator!=(const TBitField &bf) const // сравнение
-{
-  return 0;
-}
-
-TBitField TBitField::operator|(const TBitField &bf) // операция "или"
-{
-}
-
-TBitField TBitField::operator&(const TBitField &bf) // операция "и"
-{
-}
-
-TBitField TBitField::operator~(void) // отрицание
-{
-}
-
-// ввод/вывод
-
-istream &operator>>(istream &istr, TBitField &bf) // ввод
-{
-}
-
-ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
-{
-}
+//
+//// битовые операции
+//
+//TBitField& TBitField::operator=(const TBitField &bf) // присваивание
+//{
+//}
+//
+//int TBitField::operator==(const TBitField &bf) const // сравнение
+//{
+//  return 0;
+//}
+//
+//int TBitField::operator!=(const TBitField &bf) const // сравнение
+//{
+//  return 0;
+//}
+//
+//TBitField TBitField::operator|(const TBitField &bf) // операция "или"
+//{
+//}
+//
+//TBitField TBitField::operator&(const TBitField &bf) // операция "и"
+//{
+//}
+//
+//TBitField TBitField::operator~(void) // отрицание
+//{
+//}
+//
+//// ввод/вывод
+//
+//istream &operator>>(istream &istr, TBitField &bf) // ввод
+//{
+//}
+//
+//ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
+//{
+//}
